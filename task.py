5c7cdae9f21fcf25a8ab60e9127e78e45a594469
DeVon Herr <dyh@google.com>
2023-05-17 23:32:14 -0700

    Tasks now support descriptions and children

--- task.py
+++ task.py
@@ -10,9 +10,10 @@ class TaskList:
 
     def pretty_print(self):
         prettyTable = PrettyTable()
-        prettyTable.field_names = ["Task", "Name", "Urgency", "Importance"]
+        prettyTable.field_names = ["Task", "Name", "Urgency", "Importance", "Children"]
         prettyTable.align["Task"] = "r"
         prettyTable.align["Name"] = "l"
+        prettyTable.align["Children"] = "r"
         for i, task in enumerate(self.tasks):
             prettyTable.add_row(
                 [
@@ -20,24 +21,50 @@ class TaskList:
                     task.name,
                     "Urgent" if task.urgent else "",
                     "Important" if task.important else "",
+                    len(task.children),
                 ]
             )
         print(prettyTable)
 
 
 class Task:
-    def __init__(self, name="", urgent=False, important=False) -> None:
+    def __init__(
+        self, name="", urgent=False, important=False, description=None, children=None
+    ) -> None:
         self.name = name
         self.urgent = urgent
         self.important = important
 
+        if not children:
+            children = []
+        self.children = children
+
+        self.description = description
+
+    def add(self, child):
+        self.children.append(child)
+
     def __str__(self):
-        return (
-            "[Name]: {name}\n"
-            "{urgency}\n"
-            "{importance}".format(
-                name=self.name,
-                urgency="[Urgent]" if self.urgent else "[Not urgent]",
-                importance="[Important]" if self.important else "[Not important]",
-            )
-        )
+        sb = []
+
+        sb.append(f"[Name]: {self.name}")
+
+        if self.urgent:
+            sb.append("\t - Urgent")
+
+        if self.important:
+            sb.append("\t - Important")
+
+        if self.description:
+            sb.append("[Description]")
+
+            for i in range(0, len(self.description), 80):
+                sb.append(f"\t{self.description[i:i + 80]}")
+
+        if self.children:
+            sb.append("[Children]")
+
+            for child in self.children:
+                sb.append(f"\t - {child.name}")
+
+        return "\n".join(sb)
