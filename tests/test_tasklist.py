75e8a9fee25871b4cc0617a0601ae9f461c101e4
Jacob Moore <moore.jacob.s@gmail.com>
2023-05-21 16:42:52 -0700

    Use unittest framework assertions

--- tests/test_tasklist.py
+++ tests/test_tasklist.py
@@ -8,12 +8,12 @@ class TestTaskList(TestCase):
         task_list = TaskList()
         task_to_add = Task("test task")
 
-        assert len(task_list.tasks) == 0
+        self.assertEquals(len(task_list.tasks), 0)
 
         task_list.add(task_to_add)
 
-        assert len(task_list.tasks) == 1
-        assert task_list.tasks[0] == task_to_add
+        self.assertEquals(len(task_list.tasks), 1)
+        self.assertEquals(task_list.tasks[0], task_to_add)
 
     @skip("This test is not yet implemented")
     def test_list_tasks(self):
